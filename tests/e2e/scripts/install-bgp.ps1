7c0daa497fb304108243ad5900ce5829bbb46cae
vardhaman22 <vardhaman.surana@suse.com>
2026-08-10 21:42:47 +0530

    test

--- tests/e2e/scripts/install-bgp.ps1
+++ tests/e2e/scripts/install-bgp.ps1
@@ -2,8 +2,3 @@ echo "Installing RemoteAccess, RSAT-RemoteAccess-PowerShell and Routing packages
 Install-WindowsFeature RemoteAccess
 Install-WindowsFeature RSAT-RemoteAccess-PowerShell
 Install-WindowsFeature Routing
-echo "Forcing compilation of RemoteAccess WMI/CIM namespaces on Server 2025..."
-mofcomp C:\Windows\System32\wbem\RAMgmtPSProvider.mof
-Restart-Service winmgmt -Force
-echo "Installing remoteAccess vpntype: routingOnly"
-Install-RemoteAccess -VpnType RoutingOnly
